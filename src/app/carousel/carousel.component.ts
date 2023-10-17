d9912915448bacde8e7b0d5c766e02f01a2d04d7
Kartik Mishra <kartikm2219@gmail.com>
2023-10-17 11:21:54 +0530

    final commit

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -8,7 +8,7 @@ interface carouselImage {
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css'],
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
   @Input() images: carouselImage[] = [];
